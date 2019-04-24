1df292683008634ce88773e1bdd7053fd84e9ca2
csiebler <cory.siebler@gmail.com>
2019-04-23 22:56:42 -0700

    Created basic blog website

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    # ex: /blog/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /blog/1/
+    path('<int:pk>', views.DetailView.as_view(), name='detail'),
+]
